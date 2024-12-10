7cf539a92dad71d04ed230a4456320f9a5ee7dc8
bean <dbher.0010@gmail.com>
2024-12-11 05:04:49 +0900

    feat: 뭐먹뭐먹 페이지 퍼블 완료#2

---
+++ src/community/Content.jsx
@@ -0,0 +1,24 @@
+const Content = ({}) => {
+
+    return (
+        <div className={"flex justify-center mt-6"}>
+
+            <div className={"flex flex-col justify-center gap-y-5"} style={{
+                width: "520px",
+                height: "auto",
+                borderWidth: "1.5px",
+                borderStyle: "solid",
+                borderColor: "#E4E4E4",
+                borderRadius: "13.69px",
+                paddingTop: "16px",
+                paddingBottom: "16px",
+                paddingLeft: "33px",
+                paddingRight: "33px"
+            }}>
+
+            </div>
+        </div>
+    )
+}
+
+export default Content;
