547e399eb33fcb47cea458333f172a4a0ffdbe5a
bean <dbher.0010@gmail.com>
2025-02-19 15:34:27 +0900

    feat: 이미지 crud 1차 완료 #2

--- src/common/ImageViewer.jsx
+++ src/common/ImageViewer.jsx
@@ -7,7 +7,7 @@ const ImageViewer = ({images, onClickDelBtn}) => {
     return (<div className={"flex grid grid-cols-3 gap-3"}>
         {images.map((image, index) => (<div key={image.fileId}
                                      className={"w-[100px] h-[100px] border-2 flex items-center justify-center relative"}>
-            <img className={"absolute top-[-8px] right-[-8px] bg-white rounded-full"} src={deleteCircle} onClick={()=>{onClickDelBtn(index)}}/>
+            <img className={"absolute top-[-8px] right-[-8px] bg-white rounded-full cursor-pointer"} src={deleteCircle} onClick={()=>{onClickDelBtn(index)}}/>
             <img src={`${FILE_API_URL}/images/${image?.uniqueFileName}`}
                  alt={image.fileName}
                  />
